3d79131bcf07742f452cdf1a3b9a290c1c94ca7b
Max Pog <pogorelovmaxim@gmail.com>
2023-11-17 15:29:54 +0200

    Add last lesson in HTML/CSS

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,8 @@
-const modal = document.querySelector('.modal');
-const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => backdrop.classList.toggle('is-hidden');
+const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
